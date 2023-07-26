7c7332db343e0b83388f4ff409dfd1696b59edb7
Evanp215 <epybas215@gmail.com>
2023-07-25 20:32:24 -0400

    Created javascript functions to create a grid of divs, and to color said divs in on mouseover

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 const gridContainer = document.querySelector('#gridContainer');
+
+for (let i = 0; i < 256; i++) {
+ const gridSquare = document.createElement('div');
+ gridSquare.classList.add('square')
+ gridSquare.style.cssText = 'border: 1px solid black; width: 25px; height: 25px;'
+ gridContainer.appendChild(gridSquare);
+}
+
+const squares = document.querySelectorAll('.square');
+squares.forEach((square) => {
+  square.addEventListener('mouseover', () => {
+    square.style.cssText = 'border: 1px solid black; width: 25px; height: 25px; background-color: green;'
+  });
+});
