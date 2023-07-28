2c5e50d9e9e5a6727e04893179b019ba05c34742
Evanp215 <epybas215@gmail.com>
2023-07-28 09:05:44 -0400

    reworked createGrid function to allow for multiple sizes

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-const gridContainer = document.querySelector('#gridContainer');
-
-for (let i = 0; i < 256; i++) {
- const gridSquare = document.createElement('div');
- gridSquare.classList.add('square')
- gridSquare.style.cssText = 'border: 1px solid black; width: 25px; height: 25px;'
- gridContainer.appendChild(gridSquare);
-}
-
-const squares = document.querySelectorAll('.square');
-squares.forEach((square) => {
-  square.addEventListener('mouseover', () => {
-    square.style.cssText = 'border: 1px solid black; width: 25px; height: 25px; background-color: green;'
-  });
+document.addEventListener("DOMContentLoaded", function() {
+  creatGrid(32);
 });
+
+function creatGrid(size) {
+  const grid = document.querySelector('#gridContainer');
+
+  grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+}
