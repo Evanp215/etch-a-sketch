bed9da89cc8c0ccea0a4cf9c479d427c7ead9883
Evanp215 <epybas215@gmail.com>
2023-07-28 19:31:31 -0400

    Created size prompt for size button

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 document.addEventListener("DOMContentLoaded", function() {
-  creatGrid(32);
+  console.log("hello");
 });
 
-function creatGrid(size) {
+function createGrid(size) {
   const grid = document.querySelector('#gridContainer');
 
   grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
   grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+  let numDivs = size * size;
+  
+  for(let i = 0; 1 < numDivs; i++) {
+    let div = document.createElement('div');
+    grid.insertAdjacentElement('beforeend', div);
+  }
+}
+
+function getSize() {
+  let input = prompt("What will be the size of the board?");
+  let message = document.querySelector('#message')
+  if (input == "") {
+    message.innerHTML = "Please provide a number"
+  }
+  else if (input < 0 || input > 100) {
+    message.innerHTML = "Please enter a number between 1 and 100"
+  }
 }
